39498bb8a7fd1a641c7772895edba7a40d5b17ca
mrdulin <novaline@qq.com>
2018-04-13 18:55:20 +0800

    完善单元测试

--- src/modularizing-the-schema/tests/request.js
+++ src/modularizing-the-schema/tests/request.js
@@ -1,17 +1,32 @@
 const rp = require('request-promise');
 
-function request(port, body) {
-  const options = {
-    method: 'POST',
-    uri: `http://localhost:${port}/graphql`,
-    body,
-    // body: JSON.stringify(body),
-    json: true,
-    headers: {
-      'Content-Type': 'application/json'
-    }
+function RequestFactory(port) {
+  const uri = `http://localhost:${port}/graphql`;
+  function post(body) {
+    const options = {
+      method: 'POST',
+      uri,
+      body,
+      json: true,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+    return rp(options);
+  }
+
+  function get(qs) {
+    const options = {
+      uri,
+      qs,
+      json: true
+    };
+    return rp(options);
+  }
+  return {
+    post,
+    get
   };
-  return rp(options);
 }
 
-module.exports = request;
+module.exports = RequestFactory;
--- src/modularizing-the-schema/tests/server.spec.js
+++ src/modularizing-the-schema/tests/server.spec.js
@@ -1,7 +1,9 @@
 const { expect } = require('chai');
 
-const req = require('./request');
+const RequestFactory = require('./request');
 const { start, stop, port, engine } = require('../server');
+
+const req = RequestFactory(port);
 
 before(done => {
   start(done);
@@ -12,26 +14,73 @@ after(() => {
 });
 
 describe('modularizing-the-schema server test suites', () => {
-  it('should return correct author', () => {
-    const body = {
-      query: '{author(firstName: "du", lastName: "lin") { id }}'
-    };
-    return req(port, body).then(res => {
-      expect(res).to.have.nested.property('data.author.id');
-      expect(res.data.author.id).to.be.a('string');
+  describe('http post requests', () => {
+    it('should return correct author', () => {
+      const body = {
+        query: 'query {author(firstName: "du", lastName: "lin") { id }}'
+      };
+      return req.post(body).then(res => {
+        expect(res).to.have.nested.property('data.author.id');
+        expect(res.data.author.id).to.be.a('string');
+      });
+    });
+
+    it('should return all authors', () => {
+      const body = {
+        query: `{allAuthors{
+          id
+        }}`
+      };
+
+      return req.post(body).then(res => {
+        // console.log(res);
+        expect(res.data.allAuthors).to.be.an('array');
+      });
+    });
+
+    it('should return a cookie string', () => {
+      const body = {
+        query: 'query {getFortuneCookie}'
+      };
+      return req.post(body).then(res => {
+        // console.log(res);
+        expect(res.data.getFortuneCookie).to.be.a('string');
+      });
     });
   });
 
-  it('should return all authors', () => {
-    const body = {
-      query: `{allAuthors{
-        id
-      }}`
-    };
+  describe('http get requests', () => {
+    it('should return correct author', () => {
+      const body = {
+        query: 'query {author(firstName: "du", lastName: "lin") { id }}'
+      };
+      return req.get(body).then(res => {
+        expect(res).to.have.nested.property('data.author.id');
+        expect(res.data.author.id).to.be.a('string');
+      });
+    });
 
-    return req(port, body).then(res => {
-      console.log(res);
-      expect(res.data.allAuthors).to.be.an('array');
+    it('should return all authors', () => {
+      const body = {
+        query: `{allAuthors{
+          id
+        }}`
+      };
+
+      return req.get(body).then(res => {
+        // console.log(res);
+        expect(res.data.allAuthors).to.be.an('array');
+      });
+    });
+
+    it('should return a cookie string', () => {
+      const body = {
+        query: 'query {getFortuneCookie}'
+      };
+      return req.get(body).then(res => {
+        // console.log(res);
+        expect(res.data.getFortuneCookie).to.be.a('string');
+      });
     });
   });
 });
