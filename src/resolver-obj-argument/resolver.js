df9bed4ca1f112d5f548a79ca6ce08778ec4168f
mrdulin <novaline@qq.com>
2018-04-17 15:50:23 +0800

    完善resolver obj argument例子的注释

--- src/resolver-obj-argument/resolver.js
+++ src/resolver-obj-argument/resolver.js
@@ -6,6 +6,9 @@ const fakeDb = {
   }
 };
 
+// graphql中内置的5种scalar类型，分别是：
+// Int, Float, Boolean, String, ID。这里的name是String类型，不需要resolver
+
 const resolvers = {
   Query: {
     getAuthor(root, args, ctx) {
@@ -18,14 +21,20 @@ const resolvers = {
     // schema中的Author对象类型的Posts字段（也是对象类型）解析器
     // resolver的函数签名是：fieldName(obj, args, context, info) { result }
     // obj包含从父域上的解析器返回的结果的对象, 本例就是getAuthor resolver返回的结果
+    // https://www.apollographql.com/docs/graphql-tools/resolvers.html#Resolver-obj-argument
     posts: author => {
       console.log('author.posts: ', author);
       // 再通过当前author的id去查询这个author下的posts
       return fakeDb.posts[author.id];
-    }
+    },
     // 为什么没有Author对象类型的name字段解析器？
-    // 因为graphql的解析器解析到scalar类型（标量类型）就停止了，graphql中内置的5种scalar类型，分别是：
-    // Int, Float, Boolean, String, ID。这里的name是String类型，不需要resolver
+    // 因为graphql使用了默认解析器，返回obj的相应的字段的值
+    // https://www.apollographql.com/docs/graphql-tools/resolvers.html#Default-resolver
+    // 等价于
+    name: author => {
+      console.log('author.name: ', author);
+      return author.name;
+    }
   },
   // schema中的Post对象类型的resolver，和Author同理
   Post: {
